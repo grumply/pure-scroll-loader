4e4489822e89d26633c7e94686e2dd8c2618ea82
Sean Hickman <sean@grumply.com>
2020-02-13 23:35:31 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure, pure-visibility, stdenv }:
 mkDerivation {
   pname = "pure-scroll-loader";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure pure-visibility ];
   homepage = "github.com/grumply/pure-scroll-loader";
